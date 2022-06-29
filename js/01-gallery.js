2b0b1e13009957723a9194449a11b0c0b075d9af
ikudria <irakudrya@gmail.com>
2022-06-28 22:49:25 -0500

    HW-07-Task-01/02_done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,76 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+//  import * as basicLightbox from 'basiclightbox'
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+// console.log(galleryContainer);
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
+
+// 1.1.
+
+//    galleryItems.forEach((item) => {
+//         let card = `<div class="gallery__item">
+//   <a href="#" class="gallery__link">
+//     <img src="${item.preview}" alt="${item.description}" class="gallery__image">
+//   </a>
+// </div>`;
+
+//          galleryContainer.insertAdjacentHTML('beforeend', card);
+//     });
+
+// 1.2.
+
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+// console.log(cardsMarkup);
+// 2.1.
+
+galleryContainer.addEventListener("click", onGalleryImageClick);
+
+function onGalleryImageClick(event) {
+  event.preventDefault();
+
+  const orgURL = event.target.dataset.source;
+  const orgDescr = event.target.alt;
+  // console.log(orgURL);
+  // console.log(event.target.alt);
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  basicLightbox
+    .create(
+      `
+    <img src = ${orgURL} alt= ${orgDescr} width="800" height="600" /> `
+    )
+    .show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+{
+  /* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a> */
+}
+
+const galleryList = document.querySelector(".gallery");
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </li>
+  `;
+    })
+    .join("");
+}
+
+galleryList.addEventListener("click", onGalleryImageClick);
+
+function onGalleryImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
